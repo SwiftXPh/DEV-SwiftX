a2669c32396ac63fa2f8ac283a72abf5ac51b904
matteo1231 <smffronda@gmail.com>
2026-06-10 00:07:53 +0800

    feat: make birth date fields nullable in UserModel and database schema

--- SwiftX/Models/UserModel.cs
+++ SwiftX/Models/UserModel.cs
@@ -8,9 +8,9 @@ namespace SwiftX.Models
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Contact { get; set; }
-        public string BirthMonth { get; set; }
-        public string BirthDate { get; set; }
-        public string BirthYear { get; set; }
+        public string? BirthMonth { get; set; }
+        public string? BirthDate { get; set; }
+        public string? BirthYear { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
